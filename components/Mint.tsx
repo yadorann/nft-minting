024d9ff910db0e72023168e6719d49ee80d3de2d
Boseok, Seo <ggy7bbo@gmail.com>
2022-07-12 10:11:02 +0900

    Number Error fixed

--- components/Mint.tsx
+++ components/Mint.tsx
@@ -311,7 +311,7 @@ export default function Mint() {
 
                     <div className="flex items-center space-x-3">
                       <p>
-                        {Number.parseFloat(0.74).toFixed(
+                        {Number.parseFloat('0.74').toFixed(
                           2
                         )}{' '}
                         ETH
