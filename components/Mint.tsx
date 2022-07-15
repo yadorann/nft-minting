ce057d40e1cb8119fad17149cb3505daf934e8a3
yadorann <yadoran@akamir.com>
2022-07-15 20:09:06 +0900

    public

--- components/Mint.tsx
+++ components/Mint.tsx
@@ -63,10 +63,10 @@ const sales: SaleType[] = [
     amount: 2700,
     price: '60000000000000000',
     //real starttime
-    // saleStartTime: 1657891800,
-    // saleEndTime: 1657895400
-    saleStartTime: 1657879137,
-    saleEndTime: 1657886337
+    saleStartTime: 1657891800,
+    saleEndTime: 1657895400
+    // saleStartTime: 1657879137,
+    // saleEndTime: 1657886337
   },
   {
     saleType: 'PUBLIC SALE',
@@ -74,10 +74,10 @@ const sales: SaleType[] = [
     amount: 5000,
     price: '74000000000000000',
     //real starttime
-    saleStartTime: 1657895400,
-    saleEndTime: 1657899000
-    // saleStartTime: 1657879137,
-    // saleEndTime: 1657882737
+    // saleStartTime: 1657895400,
+    // saleEndTime: 1657899000
+    saleStartTime: 1657879137,
+    saleEndTime: 1657886337
   },
   {
     saleType: 'SALE END!',
